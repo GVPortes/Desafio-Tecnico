9f3600bf08935a64c21cb0bb6b5104605bd370c7
Gabriel Portes <152006861+GVPortes@users.noreply.github.com>
2024-11-15 16:29:12 -0300

    att1

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,4 +6,9 @@ const routes = new Router();
 
 routes.get('/alunos', AlunosController.index);
 
+routes.post('/alunos', AlunosController.create)
+routes.get('/alunos/:id', AlunosController.read)
+routes.put('/alunos/:id', AlunosController.update)
+routes.delete('/alunos/:id', AlunosController.delete)
+
 export default routes;
